c99c0a53e1b3fb00a67a2903ae0455700eaa86a4
Justin Kim <jjkim928@gmail.com>
2017-08-30 13:37:43 -0400

    got robot names to show up

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
+const data = require('./data.js');
 const app = express();
 
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', './views');
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-  res.send('Testing...');
+  res.render('index', data);
 })
 
 app.listen(3000, function(req, res) {
